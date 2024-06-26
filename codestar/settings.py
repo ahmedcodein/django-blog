dd7a60825ec0053e255b67998d51cdecaa2c10e9
Ahmed Al-Mudhafar <ahmed.almudhafar@gmail.com>
2024-06-26 22:41:54 +0000

    add a Post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -89,6 +89,12 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.io",
+    "https://*.herokuapp.com",
+    "https://8000-ahmedcodein-djangoblog-2oyl8tca35n.ws.codeinstitute-ide.net"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
